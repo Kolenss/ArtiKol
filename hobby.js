93ebe1d96bbcd7f02734d817ea72cd2851068846
Kolenss <collinsgt12374@gmail.com>
2025-01-04 17:53:38 +0800

    Update hobby.js

--- hobby.js
+++ hobby.js
@@ -4,7 +4,8 @@ var academic = document.getElementById('academic');
 var hobby = document.getElementById('hobby');
 var contact = document.getElementById('contact');
 var cclogo = document.getElementById('cclogo');
-
+var loginbtn = document.getElementById('loginbtn');
+var signupbtn = document.getElementById('signupbtn');
 
 cclogo.onclick = function(){
     window.location.href = "index.html";
@@ -22,6 +23,12 @@ hobby.onclick = function(){
 }
 contact.onclick = function(){
     window.location.href = "contact.html";
+}
+signupbtn.onclick = function () {
+    window.location.href = "signup.html";
+}
+loginbtn.onclick = function () {
+    window.location.href = "main.html";
 }
 
 albion.onclick = function() {
